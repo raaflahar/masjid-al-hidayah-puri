6170f76ef09118514b9ffc7250bf11a0c66e6ec7
raaflahar <raaflahar@gmail.com>
2025-01-27 21:26:33 +0700

    Update picture

--- src/pages/Documentation/Documentation.jsx
+++ src/pages/Documentation/Documentation.jsx
@@ -33,7 +33,7 @@ export default function Documentation(){
                 </div>
                 <div className='flex flex-col items-center'>
                     <div>
-                        <img src={photos[currentIndex]} alt={`Photo ${currentIndex+1}`} className="phone:w-[500px] phone:h-[300px] max-w-4xl rounded-lg shadow-lg p-2"/>
+                        <img src={photos[currentIndex]} alt={`Photo ${currentIndex+1}`} className="phone:w-[500px] phone:h-[300px] sm:max-w-4xl rounded-lg shadow-lg p-2"/>
                     </div>
                     <div className='flex justify-between mt-4'>
                         <button onClick={prevPhoto} className='px-4 py-2 bg-secondary text-white rounded-lg hover:bg-gray-800 transition duration-300 m-2 phone:text-sm'>
